f71c36e26c96854bd13c0cec9e83f9a552aef7dc
Jhonatan Holanda Maia <jhonatan_holanda@hotmail.com>
2023-06-08 17:26:57 -0300

    Introducao sobre module css

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import { Post } from './Post';
+import { Header } from './components/Header';
+
+import './styles.css';
 
 export function App() {
   return (
     <>
+      <Header />
+
       <Post 
         author="Jhonatan Maia"
         content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel qui consectetur veniam explicabo veritatis facere. Odio sit optio hic, numquam iure quis, ipsum aut beatae, excepturi nulla ipsa totam minus."
