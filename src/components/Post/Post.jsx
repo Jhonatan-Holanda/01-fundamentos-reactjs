761b4f32eac06588e193a46d946978d53949943a
Jhonatan Holanda Maia <jhonatan_holanda@hotmail.com>
2023-06-11 11:57:24 -0300

    Criando funcionalidade de remover comentario

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -34,6 +34,14 @@ export function Post({author, publishedAt, content}){
     setNewCommentText('');
   }
 
+  function deleteComment(commentToDelete) {
+    const commentsWithoutDeletedOne = comments.filter(comment => {
+      return comment != commentToDelete;
+    })
+
+    setComments(commentsWithoutDeletedOne);
+  }
+
   return(
     <article className={styles.post}>
       <header>
@@ -58,9 +66,9 @@ export function Post({author, publishedAt, content}){
       <div className={styles.content}>
         {content.map(line => {
           if(line.type === 'paragraph'){
-            return <p>{line.content}</p>
+            return <p key={line.content}>{line.content}</p>
           }else if(line.type === 'link'){
-            return <p><a href='#'>{line.content}{' '}</a></p>
+            return <p key={line.content}><a href='#'>{line.content}{' '}</a></p>
           }
         })}
       </div>
@@ -81,7 +89,7 @@ export function Post({author, publishedAt, content}){
 
       <div className={styles.commentList}>
         {comments.map(comment => {
-          return <Comment content={comment}/>
+          return <Comment key={comment} content={comment} onDeleteComment={deleteComment}/>
         })}
       </div>
     </article>
